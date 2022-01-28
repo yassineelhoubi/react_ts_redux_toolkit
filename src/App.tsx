b6036fee1ebed6d400675ec62567fc664762ec5f
yassineelhoubi <elhoubiyassine@gmail.com>
2022-01-28 21:38:03 +0100

    Redux Toolkit Slices & Store

--- src/App.tsx
+++ src/App.tsx
@@ -18,7 +18,7 @@ function App() {
           </div>
         </div>
         <div className="customer-food-container">
-          <div className="customer-food-card-container">
+          <div className=" customer-food-card-container">
             <p>Selena Gomez</p>
             <div className="customer-foods-container">
               <div className="customer-food"></div>
