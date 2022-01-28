06ec46431946a96c9e41363e235774329217be79
yassineelhoubi <elhoubiyassine@gmail.com>
2022-01-28 22:47:04 +0100

    Updating & Viewing State

---
+++ src/components/ReservationCard.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { useDispatch } from 'react-redux';
+import { removeReservation } from '../features/reservationSlice';
+
+
+interface ReservationCardTypes {
+  name: string;
+  index: number
+}
+const ReservationCard = ({ name, index }: ReservationCardTypes) => {
+  const dispatch = useDispatch()
+  return <div onClick={() => {
+    dispatch(removeReservation(index))
+  }} className="reservation-card-container">{name}</div>;
+};
+
+export default ReservationCard;
--- src/features/reservationSlice.ts
+++ src/features/reservationSlice.ts
@@ -1,6 +1,9 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 
-const initialState = {
+interface ReservationState {
+    value: string[]
+}
+const initialState: ReservationState = {
     value: []
 }
 
@@ -8,8 +11,15 @@ export const reservationsSlice = createSlice({
     name: "reservations",
     initialState,
     reducers: {
-
+        addReservation: (state, action: PayloadAction<string>) => {
+            state.value.push(action.payload)
+        },
+        removeReservation: (state, action: PayloadAction<number>) => {
+            state.value.splice(action.payload, 1)
+        }
     }
 })
+
+export const { addReservation, removeReservation } = reservationsSlice.actions
 
 export default reservationsSlice.reducer
